1ab1e69144211fe023e0d4e0967400e97ccfe470
Mahalakshmi-1603 <mahalakshmithamodaran@gmail.com>
2024-07-21 14:17:11 +0530

    Add files via upload
    Day1

---
+++ day1/FirstCode.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class FirstCode {
+
+	//main
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hi world");
+		System.out.println(8+8);
+		
+	}
+}
---
+++ day1/IfElse.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark = -0;
+		
+		if (mark>35)
+		{
+			System.out.println("Mark Is : Pass ");
+		}
+		
+		else if (mark<35)
+		{
+			System.out.println("Mark Is : Fail");
+		}
+		else
+		{
+			System.out.println("Mark is : Absent");
+		}
+
+	}
+
+}
